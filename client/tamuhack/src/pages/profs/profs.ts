24dd8e753184f498753cc707543ce6766d331ecd
Andrew Schwenn <aschwenn@verizon.net>
2019-01-26 16:00:57 -0600

    ionic start

---
+++ client/tamuhack/src/pages/profs/profs.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-profs',
+  templateUrl: 'profs.html'
+})
+export class ProfsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
