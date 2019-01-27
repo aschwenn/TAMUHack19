5127cde37bcfb03ccb96f6178c3bebb83c369cdb
Alex Le <kawoofish@gmail.com>
2019-01-26 18:51:37 -0600

    Regex for prof, GPA, grades, and course separated out into lists, ready for processing

--- testing/main.py
+++ testing/main.py
@@ -8,9 +8,28 @@ def parsePDF(path):
         print('Page type : {}'.format(str(type(page))))
 
         text = page.extractText().encode('ascii', 'ignore')
+        text = cleanText(text)
+        text = cleanHeader(text)
+        extractProf(text)
         extractCourses(text)
         extractGrades(text)
-        print(text)
+        extractGPA(text)
+        # print(text)
+
+def cleanText(text):
+    # Remove Course Total statistics
+    regex = "(COURSE TOTAL:[\s\S]*?(?=[A-Z]))"
+    output = re.sub(regex, '', text)
+    regex = "([0-9]+\.[0-9]+[%]\\n)"
+    output = re.sub(regex, '', output)
+    # print(output)
+    return output
+
+def cleanHeader(text):
+    regex = "(SECTION[\s\S]*?Undergraduate\\n)|(SECTION[\s\S]*?Graduate\\n)"
+    output = re.sub(regex, '', text)
+    print(output)
+    return output
 
 def extractCourses(text):
     regex = "([A-Z]{4}-[0-9]{3}-[0-9]{3})+"
@@ -19,8 +38,18 @@ def extractCourses(text):
 
 
 def extractGrades(text):
-    reg = "(?<![-\.]|[0-9])([0-9]{1,3}(?![-\%\.\d]))"
-    output = re.findall(reg, text)
+    regex = "(?<![-\.]|[0-9])([0-9]{1,3}(?![-\d]))"
+    output = re.findall(regex, text)
+    print(output)
+
+def extractGPA(text):
+    regex = "[0-9]{1}\.[0-9]{3}"
+    output = re.findall(regex, text)
+    print(output)
+
+def extractProf(text):
+    regex = "(?!\\n)[A-Z\s]+(?=\\n)"
+    output = re.findall(regex, text)
     print(output)
 
 
