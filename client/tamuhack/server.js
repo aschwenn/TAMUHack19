12b5c4efeaef67e8f355959bbf15ae68308f4cf3
Andrew Schwenn <aschwenn@verizon.net>
2019-01-26 18:04:13 -0600

    initialized server

---
+++ client/tamuhack/server.js
@@ -0,0 +1,7 @@
+// Imports
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mysql = require('mysql');
+var prompt = require('prompt');
+
